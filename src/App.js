a27fdce237cbdb9934cd548bd17e7e10414a8862
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2022-12-28 11:53:11 +0600

    task-item-shown-on-ui

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import { Container } from "@material-ui/core";
 import NavBar from "./components/AppBar/NavBar";
 import InputField from "./components/InputField/InputField";
 import TaskItem from "./components/TaskItem/TaskItem";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setTasks(data));
+  }, [tasks]);
   return (
     <Container>
       <NavBar></NavBar>
       <InputField></InputField>
-      {[1, 2, 3, 4].map((indx, item) => (
-        <TaskItem key={indx} item={item}></TaskItem>
+      {tasks.map((item) => (
+        <TaskItem key={item._id} item={item}></TaskItem>
       ))}
     </Container>
   );
--- src/components/TaskItem/TaskItem.js
+++ src/components/TaskItem/TaskItem.js
@@ -5,6 +5,7 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Checkbox from "@material-ui/core/Checkbox";
 import Button from "@material-ui/core/Button";
+
 const useStyles = makeStyles((theme) => ({
   paper: {
     boxShadow: "none",
@@ -24,6 +25,7 @@ const TaskItem = ({ item }) => {
       alignItems="center"
       style={{
         marginTop: "10px",
+        marginBottom: "10px",
         padding: "10px",
         boxShadow:
           "0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)",
@@ -33,13 +35,14 @@ const TaskItem = ({ item }) => {
       <Grid style={{ alignItems: "center" }}>
         <Paper className={classes.paper}>
           <FormControlLabel
+            checked={item?.isChecked}
             control={<Checkbox name="checkedA" />}
-            label={`Task Item ${item + 1}`}
+            label={item?.name}
           />
         </Paper>
       </Grid>
       <Grid>
-        <Button style={{ marginRight: "3px" }} color="primary">
+        <Button style={{ marginRight: "5px" }} color="primary">
           EDIT
         </Button>
         <Button color="primary">DELETE</Button>
